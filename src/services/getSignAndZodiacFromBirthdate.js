d61ece16cce1969c3a0f0baf74df0d2b44208d6e
Fievez Alexandre <fievezalexandre.git@gmail.com>
2024-01-30 17:00:49 +0100

    feat(add-astrological-sign): add astrologic sign

--- src/services/getSignAndZodiacFromBirthdate.js
+++ src/services/getSignAndZodiacFromBirthdate.js
@@ -1,13 +1,35 @@
-const { getZodiac } = require("horoscope");
+const { getZodiac, getSign } = require("horoscope");
 
 module.exports = {
   getZodiacFromYear: (req, res) => {
-    const birthYear = Number(req.params.birthYear);
+    try {
+      const birthYear = Number(req.params.birthYear);
 
-    if (isNaN(birthYear) || birthYear <= 0 || birthYear > 9999) {
-      console.log("Année introuvable");
-    } else {
-      return res.json({ zodiac: getZodiac(birthYear) });
+      if (isNaN(birthYear) || birthYear <= 0 || birthYear > 9999) {
+        console.log("Année introuvable");
+      } else {
+        return res.json({ zodiac: getZodiac(birthYear) });
+      }
+    } catch (e) {
+      console.log("erreur :" + e);
+    }
+  },
+  getAstrologicalSignFromMonthAndDay: (req, res) => {
+    try {
+      const birthMonth = Number(req.params.birthMonth);
+      const birthDay = Number(req.params.birthDay);
+
+      if (isNaN(birthMonth) || birthMonth < 0 || birthMonth > 12) {
+        console.log("Le mois est invalide");
+      } else if (isNaN(birthDay) || birthDay < 0 || birthDay > 31) {
+        console.log("Le jours est invalide");
+      } else {
+        return res.json({
+          astrologicalSign: getSign({ month: birthMonth, day: birthDay }),
+        });
+      }
+    } catch (e) {
+      console.log("erreur: " + e);
     }
   },
 };
