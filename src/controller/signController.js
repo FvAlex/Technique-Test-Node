faeb484e1346036de462cf350947de56a5c7928f
Fievez Alexandre <77385048+FvAlex@users.noreply.github.com>
2024-01-30 17:01:28 +0100

    feat(add-astrological-sign): add astrologic sign (#4)

--- src/controller/signController.js
+++ src/controller/signController.js
@@ -1,9 +1,14 @@
 const {
   getZodiacFromYear,
+  getAstrologicalSignFromMonthAndDay,
 } = require("./../services/getSignAndZodiacFromBirthdate");
 
 module.exports = {
   GetZodiacFromYearController: (req, res, next) => {
     return getZodiacFromYear(req, res);
+  },
+
+  GetAstrologicalSignFromMonthAndDayController: (req, res, next) => {
+    return getAstrologicalSignFromMonthAndDay(req, res);
   },
 };
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,11 +1,16 @@
 const {
   GetZodiacFromYearController,
+  GetAstrologicalSignFromMonthAndDayController,
 } = require("./../controller/signController");
 
 function routes() {
   this.init = function (app) {
     app.get("/api/zodiac/:birthYear", function (req, res, next) {
       return GetZodiacFromYearController(req, res, next);
+    });
+
+    app.get("/api/sign/:birthMonth/:birthDay", function (req, res, next) {
+      return GetAstrologicalSignFromMonthAndDayController(req, res, next);
     });
 
     app.all(/.*/, function (req, res, next) {
