9ddc18336314b0fa0faf1e4241c9d329c8b747e8
Fievez Alexandre <fievezalexandre.git@gmail.com>
2024-01-30 17:32:05 +0100

    feat(add-prettier): add prettier

--- src/services/getSignAndZodiacFromBirthdate.js
+++ src/services/getSignAndZodiacFromBirthdate.js
@@ -1,46 +1,42 @@
-const { getZodiac, getSign } = require("horoscope");
-const CustomError = require("../errors/customError");
-const {
-  INCORRECT_YEAR,
-  INCORRECT_MONTH,
-  INCORRECT_DAY,
-} = require("../errors/errorCodes");
+const { getZodiac, getSign } = require('horoscope')
+const CustomError = require('../errors/customError')
+const { INCORRECT_YEAR, INCORRECT_MONTH, INCORRECT_DAY } = require('../errors/errorCodes')
 
 module.exports = {
   getZodiacFromYear: (req, res) => {
     try {
-      const birthYear = Number(req.params.birthYear);
+      const birthYear = Number(req.params.birthYear)
 
       if (isNaN(birthYear) || birthYear <= 0 || birthYear > 9999) {
-        console.log("Année introuvable");
-        throw new CustomError(INCORRECT_YEAR);
+        console.log('Année introuvable')
+        throw new CustomError(INCORRECT_YEAR)
       } else {
-        return res.json({ zodiac: getZodiac(birthYear) });
+        return res.json({ zodiac: getZodiac(birthYear) })
       }
     } catch (e) {
-      console.log(e);
-      return res.status(500).json({ error: e.message });
+      console.log(e)
+      return res.status(500).json({ error: e.message })
     }
   },
   getAstrologicalSignFromMonthAndDay: (req, res) => {
     try {
-      const birthMonth = Number(req.params.birthMonth);
-      const birthDay = Number(req.params.birthDay);
+      const birthMonth = Number(req.params.birthMonth)
+      const birthDay = Number(req.params.birthDay)
 
       if (isNaN(birthMonth) || birthMonth < 0 || birthMonth > 12) {
-        console.log("Le mois est invalide");
-        throw new CustomError(INCORRECT_MONTH);
+        console.log('Le mois est invalide')
+        throw new CustomError(INCORRECT_MONTH)
       } else if (isNaN(birthDay) || birthDay < 0 || birthDay > 31) {
-        console.log("Le jours est invalide");
-        throw new CustomError(INCORRECT_DAY);
+        console.log('Le jours est invalide')
+        throw new CustomError(INCORRECT_DAY)
       } else {
         return res.json({
-          astrologicalSign: getSign({ month: birthMonth, day: birthDay }),
-        });
+          astrologicalSign: getSign({ month: birthMonth, day: birthDay })
+        })
       }
     } catch (e) {
-      console.log(e);
-      return res.status(500).json({ error: e.message });
+      console.log(e)
+      return res.status(500).json({ error: e.message })
     }
-  },
-};
+  }
+}
